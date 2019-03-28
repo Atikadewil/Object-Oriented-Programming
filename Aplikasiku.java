3f8adfe96f2617f4b004eb367c894a2cda511c1c
Atikadewil <48239066+Atikadewil@users.noreply.github.com>
2019-03-28 13:03:43 +0700

    Add files via upload

--- Aplikasiku.java
+++ Aplikasiku.java
@@ -5,10 +5,36 @@
  */
 package entitas;
 
-/**
- *
- * @author User
- */
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Aplikasiku {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
     
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Halo");
+        frame.setSize(300, 200);
+        
+        frame.setLayout(new BorderLayout());
+        
+        JLabel lblNama = new JLabel();
+        lblNama.setText("Atika dewi lestari"
+                        + " 4D/17090133");
+        frame.add(lblNama, BorderLayout.WEST);
+        
+        JButton btnSimpan = new JButton("Simpan");
+        frame.add(btnSimpan, BorderLayout.EAST);
+        
+        frame.setVisible(true);
+    }
+     
 }
+
